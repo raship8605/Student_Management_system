bfbb8744beaa5893b8f6c52cd93dd7e360f564d1
rashi <rashipardeshi870@gmail.com>
2026-05-12 15:23:55 +0530

    Initial commit

---
+++ student.py
@@ -0,0 +1,12 @@
+# class Student:
+#     all_students=[]
+#     def __init__(self,name,roll_number,marks):
+#         self.name=name
+#         self.roll_number=roll_number
+#         self.marks=marks
+#     def menu():
+#         print("\n==========Student Managenemnt system==========")
+#         print("1. Add student")
+#         print("2. Update Marks")
+#         print("3. Show all Student")
+#         print("4. Exit")

56af1d48294c4eebdaa99e29b98d2e4a2a5a4782
rashi <rashipardeshi870@gmail.com>
2026-05-12 15:37:47 +0530

    Created simple structure of system

--- student.py
+++ student.py
@@ -1,12 +1,36 @@
-# class Student:
-#     all_students=[]
-#     def __init__(self,name,roll_number,marks):
-#         self.name=name
-#         self.roll_number=roll_number
-#         self.marks=marks
-#     def menu():
-#         print("\n==========Student Managenemnt system==========")
-#         print("1. Add student")
-#         print("2. Update Marks")
-#         print("3. Show all Student")
-#         print("4. Exit")
+class Student:
+    all_students=[]
+    def __init__(self,name,roll_number,marks):
+        self.name=name
+        self.roll_number=roll_number
+        self.marks=marks
+
+    # @classmethod
+    # def add_student(cls):
+
+
+def menu():
+        while True:
+            print("\n==========Student Managenemnt system==========")
+            print("1. Add student")
+            print("2. Update Marks")
+            print("3. Show all Student")
+            print("4. Exit")
+
+            choice=int(input("Enter your option(1-4): "))
+            if choice=="1":
+                Student.add_student()
+            elif choice=="2":
+                Student.update_student_marks()
+            elif choice=="3":
+                Student.show_all_students()
+            elif choice=="4":
+                print("Exiting Student Management system, Bye!!")
+                break
+            else:
+                print("Invalid choice, please try again.")
+
+if __name__=="__main__":
+    menu()
+
+        
